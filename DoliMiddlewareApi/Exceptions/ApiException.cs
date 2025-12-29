4cb4653e3b3ae0b16dd1cd963ffb1007d732722d
javiermengual <javiermengual@live.com>
2025-12-29 16:37:46 +0100

    feat: añadir excepciones personalizadas para la API

---
+++ DoliMiddlewareApi/Exceptions/ApiException.cs
@@ -0,0 +1,9 @@
+namespace DoliMiddlewareApi.Exceptions;
+
+public class ApiException : Exception
+
+{
+    public ApiException(string message) : base(message)
+    {
+    }
+}
---
+++ DoliMiddlewareApi/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace DoliMiddlewareApi.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+    }
+}
