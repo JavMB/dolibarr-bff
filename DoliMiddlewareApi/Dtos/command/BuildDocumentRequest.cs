0e7bee7e43ce898075c10bf72ad69eec8ecf42ab
JM <jmengual@consoft.es>
2026-04-15 16:29:05 +0200

    empiezo el servicio para descarga de pdfs

---
+++ DoliMiddlewareApi/Dtos/command/BuildDocumentRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace DoliMiddlewareApi.Dtos.command
+{
+    public class BuildDocumentRequest
+    {
+        public string modulepart { get; set; } = default!;
+        public string? original_file { get; set; }
+        public string? doctemplate { get; set; }
+        public string? langcode { get; set; }
+
+
+    }
+}
---
+++ DoliMiddlewareApi/Services/DocumentService.cs
@@ -0,0 +1,23 @@
+﻿using DoliMiddlewareApi.Dtos.command;
+using DoliMiddlewareApi.Services.Clients;
+
+namespace DoliMiddlewareApi.Services
+{
+    public class DocumentService(IDolibarrApiClient apiClient)
+    {
+
+        public async Task BuildInvoicePdfAsync(string invoiceRef)
+        {
+            var request = new BuildDocumentRequest
+            {
+                modulepart = "invoice",
+                original_file = $"{invoiceRef}/{invoiceRef}.pdf",
+                doctemplate = "crabe",
+                langcode = "es_ES"
+            };
+
+            await apiClient.PutAsync("documents/builddoc",request);
+        }
+        
+    }
+}
